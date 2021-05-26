46a09c5ac96eb2f9f7bfae4fee56e19e2e56fbf6
June <juneandkang@gmail.com>
2021-05-26 18:32:05 +0900

    Add: 수정요청사항 반영

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Router, { Switch } from "react-router-dom";
+import React from 'react';
+import Router, { Switch } from 'react-router-dom';
 
 import LoginBaek from './pages/jinsubaek/Login/Login';
 import MainBaek from './pages/jinsubaek/Main/Main';
@@ -16,25 +15,22 @@ import MainKim from './pages/mingikim/Main/Main';
 
 // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
 class Router extends React.Component {
-    render(){
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path='/login-baek' component={LoginBaek} />
-                    <Route exact path='/main-baek' component={MainBaek} />
-
-                    <Route exact path='/login-kang' component={LoginKang} />
-                    <Route exact path='/main-kang' component={MainKang} />
-
-                    <Route exact path='/login-jung' component={LoginJung} />
-                    <Route exact path='/main-jung' component={MainJung} />
-                    
-                    <Route exact path='/login-kim' component={LoginKim} />
-                    <Route exact path='/main-kim' component={MainKim} />
-                </Switch>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/login-baek" component={LoginBaek} />
+          <Route exact path="/main-baek" component={MainBaek} />
+          <Route exact path="/login-kang" component={LoginKang} />
+          <Route exact path="/main-kang" component={MainKang} />
+          <Route exact path="/login-jung" component={LoginJung} />
+          <Route exact path="/main-jung" component={MainJung} />
+          <Route exact path="/login-kim" component={LoginKim} />
+          <Route exact path="/main-kim" component={MainKim} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
-ReactDOM.render(<Router />,document.getElementById("root"));
+export default Router;
